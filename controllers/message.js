1a71b8499f0501b9d1dd8fc1af3e0b580241ed40
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-03-06 12:54:49 -0800

    add form field for sending an email

--- controllers/message.js
+++ controllers/message.js
@@ -2,23 +2,9 @@ Emailer.MessageController = Ember.Controller.extend({
   messageSent: false,
   actions: {
     sendMessage: function() {
-      var message = prompt('Type your email here:');
+      var message =
       this.set('confirmationNumber', Math.round(Math.random() * 100000));
       this.set('messageSent', true);
     }
   }
 });
-// Emailer.NewEmailController = Ember.Controller.extend({
-//   actions: {
-//     save: function() {
-//       var newEmail = this.store.createRecord('email', {
-//         address: this.get('address'),
-//         subject: this.get('subject'),
-//         body: this.get('body')
-//       });
-//       debugger;
-//       newEmail.save();
-//       this.transitionToRoute('emails');
-//     }
-//   }
-// })
